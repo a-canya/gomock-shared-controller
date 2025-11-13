f4cdda6df7015a798d91c8f65aee0bf6cdf649e5
Arnau <arnau.canyadell@estudiantat.upc.edu>
2025-11-14 00:50:20 +0100

    tweak

--- a.go
+++ a.go
@@ -1,6 +1,6 @@
 package gomocksharedcontroller
 
-//go:generate mockgen -destination=mock_test.go -package=gomocksharedcontroller . I
+//go:generate mockgen -destination=mock.go -package=gomocksharedcontroller . I
 
 type I interface {
 	M()
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module gomock-shared-controller
 
 go 1.25.4
 
-require go.uber.org/mock v0.6.0 // indirect
+require go.uber.org/mock v0.6.0
